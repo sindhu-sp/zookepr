4e295cb6132fa9048896c62c76632151f2615396
Sindhu Pillai <sindhupillai@Sindhus-MacBook-Pro.local>
2021-07-18 23:40:28 -0700

    added get and post

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const PORT = process.env.PORT || 5001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({extended:true }));
+// parse incoming JSON data
+app.use(express.json());
 
 const { animals } = require('./data/animals');
 
@@ -30,6 +38,37 @@ const { animals } = require('./data/animals');
 //       }
 //       return filteredResults
 //   }
+  
+
+  app.get('/api/animals/:id', (req, res) => {
+      const result = findById(req.params.id, animals);
+      if(result){
+      res.json(result);
+      }
+      else {
+        res.send(404);
+    }
+  });
+
+  app.post('/api/animals', (req, res) => {
+      //req body is where our incoming content will be
+      // set id based on what the next index of the array will be
+      req.body.id = animals.length.toString();
+      
+    //   if any data in the req.body is incorrect, sned 400 error back
+    if(validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted');
+    } else {
+
+      //ad animal to json file and animals array in the function
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
+    }
+
+  });
+
+
+
   function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -69,19 +108,35 @@ const { animals } = require('./data/animals');
     return filteredResults;
   }
 
-  app.get('/api/animals/:id', (req, res) => {
-      const result = findById(req.params.id, animals);
-      if(result){
-      res.json(result);
-      }
-      else {
-        res.send(404);
-    }
-  });
-
   function findById(id, animalsArray){
       const result = animalsArray.filter(animal => animal.id === id)[0];
       return result;
+  }
+
+  function createNewAnimal(body, animalsArray) {
+      const animal = body;
+      animalsArray.push(animal);
+      fs.writeFileSync(path.join(__dirname, './data/animals.json'),
+      JSON.stringify({animals: animalsArray}, null, 2)
+      );
+
+      return animal;
+  }
+
+  function validateAnimal(animal) {
+      if (!animal.name || typeof animal.name !== 'string') {
+          return false;
+      }
+      if (!animal.species || typeof animal.species !== "string") {
+          return false;
+      }
+      if (!animal.diet || typeof animal.diet !== "string") {
+          return false;
+      }
+      if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+          return false;
+      }
+      return true;
   }
   
 // App Listener
