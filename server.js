61eb2a625f472f68ca67467bd960e94efff03b8a
Sindhu Pillai <sindhupillai@Sindhus-MacBook-Pro.local>
2021-07-19 21:06:42 -0700

    files added and changes made

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({extended:true }));
 // parse incoming JSON data
@@ -139,6 +140,18 @@ const { animals } = require('./data/animals');
       return true;
   }
   
+  app.get('/', (req, res) => {
+      res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 // App Listener
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
